d95e26bef7d67b44d0b25c6256c7b79207d48de1
markhilkert <mark.hilkert@gmail.com>
2019-01-24 15:00:37 -0600

    added url searches for the user

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,16 @@
 class Contact < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: true
+
+
+
   def friendly_updated_at
     updated_at.strftime("%b %d, %Y")
   end
 
   def full_name
-    first_name + " " + middle_name + " " + last_name
+    first_name + " " + (middle_name || "") + " " + last_name
   end
 
   def turning_japanese
